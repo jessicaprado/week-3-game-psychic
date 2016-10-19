4c4e907704da083cae0a2a7a24d013e952f65faf
Jessica Prado <jessica.marie.prado@gmail.comgit config --global user.name Jessica>
2016-10-19 14:18:03 -0400

    Justin help again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 
 // alphabet
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Scoreboard variables
 var wins = 0;
 var losses = 0;
 var guessesLeft = 8;
-var guessesSoFar = ""
+var guessesSoFar = [];
 
 
 //computer selects random letter from array
@@ -20,33 +21,46 @@ var computerGuess = letterChoice(alphabet);
 console.log(computerGuess);
 
 //User guess
+
+
+
+ 
+function displayLetters(userGuess , guessesSoFar) {	
+	
+	guessesSoFar.push(userGuess);
+	 
+	//var lettersGuessed = displayLetters(guessesSoFar, userGuess);
+	console.log(guessesSoFar);
+
+}
+
 document.onkeyup = function(event) {
 	
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();	
-	console.log(userGuess);
+	//console.log(userGuess);
+
+
 
 	//Log and Display user guesses on html
 	
 		if(userGuess !== computerGuess) {
 				guessesLeft--;
-					if(guessesLeft == 0 ) {
-						losses++;
-						guessesLeft = 8;
-						computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-						console.log(computerGuess);
-					}
-				} 
-			else if(userGuess = computerGuess) {
-				wins++;
+			if(guessesLeft == 0 ) {
+				losses++;
 				guessesLeft = 8;
-				alert("You got it!");
 				computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 				console.log(computerGuess);
-				}
+			}
+		} else if(userGuess = computerGuess) {
+			wins++;
+			guessesLeft = 8;
+			alert("You got it!");
+			computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+			console.log(computerGuess);
+		}
 
+	displayLetters(userGuess , guessesSoFar);
 
-
-	//Scoreboard
 	var score = 
 	"<p>Guess what letter I'm thinking of" +
 	"<p>wins: " + wins + 
@@ -58,5 +72,9 @@ document.onkeyup = function(event) {
 	//"</p>";
 
 	document.getElementById("game").innerHTML = score;
-	
+
 }
+
+	//Scoreboard
+
+	
