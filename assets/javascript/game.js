5d959d9e3defd894e1396a1da58b25a151afa1c9
Jessica Prado <jessica.marie.prado@gmail.comgit config --global user.name Jessica>
2016-10-19 15:32:37 -0400

    new changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@
 // alphabet
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var alphabet1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 // Scoreboard variables
 var wins = 0;
 var losses = 0;
@@ -21,60 +20,54 @@ var computerGuess = letterChoice(alphabet);
 console.log(computerGuess);
 
 //User guess
-
-
-
- 
-function displayLetters(userGuess , guessesSoFar) {	
-	
-	guessesSoFar.push(userGuess);
-	 
-	//var lettersGuessed = displayLetters(guessesSoFar, userGuess);
-	console.log(guessesSoFar);
-
+function displayLetters(userGuess , guessesSoFar) {		
+	guessesSoFar.push(userGuess);			
 }
 
-document.onkeyup = function(event) {
-	
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();	
-	//console.log(userGuess);
+document.onkeyup = function(event) {	
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 
 
-	//Log and Display user guesses on html
-	
+	//Log and Display user guesses on html	
 		if(userGuess !== computerGuess) {
 				guessesLeft--;
+				alphabet.splice(userGuess);	
+				console.log(alphabet);		
+			}				
+				
 			if(guessesLeft == 0 ) {
 				losses++;
 				guessesLeft = 8;
-				computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-				console.log(computerGuess);
-			}
-		} else if(userGuess = computerGuess) {
+				computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];	
+				guessesSoFar = []; 	
+
+			
+		} else if(userGuess == computerGuess) {
 			wins++;
 			guessesLeft = 8;
-			alert("You got it!");
+			alert("You got it!");			
 			computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+			guessesSoFar = []; 
 			console.log(computerGuess);
 		}
 
-	displayLetters(userGuess , guessesSoFar);
+	displayLetters(userGuess, guessesSoFar);
 
+	//Scoreboard
 	var score = 
 	"<p>Guess what letter I'm thinking of" +
 	"<p>wins: " + wins + 
 	"</p>" +
 	"<p>losses: " + losses + 
 	"</p>" +
-	"<p>Guesses left: " + guessesLeft; 
-	//"<p>Your Guesses so far: " + guessesSoFar + 
-	//"</p>";
+	"<p>Guesses left: " + guessesLeft +
+	"<p>Your Guesses so far: " + guessesSoFar;
 
 	document.getElementById("game").innerHTML = score;
 
 }
 
-	//Scoreboard
+
 
 	
